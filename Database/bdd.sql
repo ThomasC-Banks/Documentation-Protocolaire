fa6d5c77154cccdcc1563dc7b3a4fe30f85c3994
CThomas <corde.thomas1@gmail.com>
2025-11-24 13:27:54 +0100

    feat: bdd

---
+++ Database/bdd.sql
@@ -0,0 +1,17 @@
+-- Création de la BDD --
+
+CREATE DATABASE IF NOT EXISTS users_management;
+USE users_management;
+
+-- Création de la table: Users --
+
+CREATE TABLE users (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    mail VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    birth_date DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
